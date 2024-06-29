f59b4df6b32180a8eeebdb5709671fc6c2f52e5e
luigi-raynel-dev <devluigiraynel@gmail.com>
2024-06-29 00:03:42 -0300

    feat: config metro

--- App.tsx
+++ App.tsx
@@ -1,59 +1,81 @@
-import { NativeBaseProvider } from 'native-base'
-import { Alert, StatusBar } from 'react-native'
-import {
-  useFonts,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_700Bold
-} from '@expo-google-fonts/open-sans'
-import { THEME } from './src/styles/theme'
-import Loading from './src/components/Loading'
-import Routes from './src/routes'
-import { AuthContextProvider } from './src/context/AuthContext'
-import { IntroContextProvider } from './src/context/IntroContext'
-import { PushNotificationContextProvider } from './src/context/PushNotificationContext'
-import messaging from '@react-native-firebase/messaging'
-import * as Clipboard from 'expo-clipboard'
+// import { NativeBaseProvider } from 'native-base'
+// import { Alert, StatusBar } from 'react-native'
+// import {
+//   useFonts,
+//   OpenSans_400Regular,
+//   OpenSans_500Medium,
+//   OpenSans_700Bold
+// } from '@expo-google-fonts/open-sans'
+// import { THEME } from './src/styles/theme'
+// import Loading from './src/components/Loading'
+// import Routes from './src/routes'
+// import { AuthContextProvider } from './src/context/AuthContext'
+// import { IntroContextProvider } from './src/context/IntroContext'
+// import { PushNotificationContextProvider } from './src/context/PushNotificationContext'
+// import messaging from '@react-native-firebase/messaging'
+// import * as Clipboard from 'expo-clipboard'
 
+import { Text, View } from 'react-native'
+
+// export default function App() {
+//   const [fonstLoaded] = useFonts({
+//     OpenSans_400Regular,
+//     OpenSans_500Medium,
+//     OpenSans_700Bold
+//   })
+
+//   messaging().setBackgroundMessageHandler(async remoteMessage => {
+//     Alert.alert(
+//       'Mensagem recebida em background:',
+//       JSON.stringify(remoteMessage),
+//       [
+//         {
+//           text: 'Copiar',
+//           onPress: async () => {
+//             await Clipboard.setStringAsync(JSON.stringify(remoteMessage))
+//           }
+//         },
+//         { text: 'Fechar', onPress: () => console.log('Fechar Pressionado') }
+//       ]
+//     )
+//   })
+
+//   return !fonstLoaded ? (
+//     <Loading />
+//   ) : (
+//     <NativeBaseProvider theme={THEME}>
+//       <IntroContextProvider>
+//         <AuthContextProvider>
+//           <PushNotificationContextProvider>
+//             <StatusBar
+//               barStyle="light-content"
+//               backgroundColor="transparent"
+//               translucent
+//             />
+//             <Routes />
+//           </PushNotificationContextProvider>
+//         </AuthContextProvider>
+//       </IntroContextProvider>
+//     </NativeBaseProvider>
+//   )
+// }
 export default function App() {
-  const [fonstLoaded] = useFonts({
-    OpenSans_400Regular,
-    OpenSans_500Medium,
-    OpenSans_700Bold
-  })
-
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    Alert.alert(
-      'Mensagem recebida em background:',
-      JSON.stringify(remoteMessage),
-      [
-        {
-          text: 'Copiar',
-          onPress: async () => {
-            await Clipboard.setStringAsync(JSON.stringify(remoteMessage))
-          }
-        },
-        { text: 'Fechar', onPress: () => console.log('Fechar Pressionado') }
-      ]
-    )
-  })
-
-  return !fonstLoaded ? (
-    <Loading />
-  ) : (
-    <NativeBaseProvider theme={THEME}>
-      <IntroContextProvider>
-        <AuthContextProvider>
-          <PushNotificationContextProvider>
-            <StatusBar
-              barStyle="light-content"
-              backgroundColor="transparent"
-              translucent
-            />
-            <Routes />
-          </PushNotificationContextProvider>
-        </AuthContextProvider>
-      </IntroContextProvider>
-    </NativeBaseProvider>
+  return (
+    <View>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+      <Text>Olá mundo</Text>
+    </View>
   )
 }
--- android/app/src/main/java/com/devluigiraynel/expendia/MainApplication.kt
+++ android/app/src/main/java/com/devluigiraynel/expendia/MainApplication.kt
@@ -31,7 +31,7 @@ class MainApplication : Application(), ReactApplication {
               )
           }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+          override fun getJSMainModuleName(): String = "index"
 
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
